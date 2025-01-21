6f937ee4e8cda870cebe63476883e46955058ee7
rauf <becario1.sis@grupotarahumara.com.mx>
2025-01-21 13:56:49 -0500

    start date  and end date

--- tryconnection.js
+++ tryconnection.js
@@ -160,13 +160,13 @@ app.post('/actualizarCurso', (req, res) => {
 
 app.post('/agregarCurso', (req, res) => {
   
-  const { title, description, area, tutor } = req.body;
+  const { title, description, area, tutor,start_date ,end_date } = req.body;
  
   // Verifica si los datos se están recibiendo correctamente
   console.log("Datos recibidos:", req.body);
  
-  const query = `INSERT INTO cursos_presenciales (title, description, area, tutor,status)
-                 VALUES ('${title}', '${description}', '${area}', '${tutor}',"true")`;
+  const query = `INSERT INTO cursos_presenciales (title, description, area, tutor,start_date,end_date,status)
+                 VALUES ('${title}', '${description}', '${area}', '${tutor}','${start_date} ','${end_date} ',"true")`;
  
   try {
     db.query(query, (err, result) => {
