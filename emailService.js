4ed69ffdc149b68618ca14670b3949507f2dbea2
Francisco Castellanos <juan.castellanos@grupotarahumara.com.mx>
2025-04-29 15:23:24 -0600

    Env

--- emailService.js
+++ emailService.js
@@ -1,30 +1,34 @@
 // emailService.js
+
 import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'movimientospersonal.noreply@gmail.com',
-      pass: 'tubi ajdt grjl akpz'
-    }
-  });
-
-  export async function enviarCorreo(destinatario, asunto, contenidoHTML) {
-    try {
-      console.log("📧 Preparando envío de correo...");
-      console.log("📍 Destinatario:", destinatario);
-      console.log("📍 Asunto:", asunto);
-      console.log("📍 Contenido HTML:", contenidoHTML);
-  
-      const info = await transporter.sendMail({
-        from: '"Sistema de Aprobaciones" <movimientospersonal.noreply@gmail.com>',
-        to: destinatario,
-        subject: asunto,
-        html: contenidoHTML
-      });
-  
-      console.log("✅ Correo enviado:", info.response);
-    } catch (error) {
-      console.error("❌ Error al enviar correo:", error);
-    }
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   }
+});
+
+export async function enviarCorreo(destinatario, asunto, contenidoHTML) {
+  try {
+    console.log("📧 Preparando envío de correo...");
+    console.log("📍 Destinatario:", destinatario);
+    console.log("📍 Asunto:", asunto);
+    console.log("📍 Contenido HTML:", contenidoHTML);
+
+    const info = await transporter.sendMail({
+      from: `"Sistema de Aprobaciones" <${process.env.EMAIL_USER}>`,
+      to: destinatario,
+      subject: asunto,
+      html: contenidoHTML
+    });
+
+    console.log("✅ Correo enviado:", info.response);
+  } catch (error) {
+    console.error("❌ Error al enviar correo:", error);
+  }
+}
