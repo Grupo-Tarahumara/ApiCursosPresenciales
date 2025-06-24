e6ba4acfa9991ded5fcfa9c07cb513e14dde88ff
Francisco Castellanos <juan.castellanos@grupotarahumara.com.mx>
2025-06-24 13:44:07 -0600

    Frontend url

--- tryconnection.js
+++ tryconnection.js
@@ -29,9 +29,11 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = process.env.SERVER_PORT || 3041;
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true, // solo si usas cookies o sesiones
+}));
 app.use('/public', express.static('public'));
-
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
